078f7b7a603e9923e599532055d07788cba65ff0
Ed-Davis <skootaman@btopenworld.com>
2024-03-08 05:41:44 +0000

    Updating comment

--- apilib.py
+++ apilib.py
@@ -5,7 +5,7 @@ from dotenv import dotenv_values
 
 
 class Api:
-    """This class encapsulates the data retrieval and sorting"""
+    """This class encapsulates the data retrieval and sorting, and the mock option"""
 
     def __init__(self) -> None:
         """Constructor method"""
