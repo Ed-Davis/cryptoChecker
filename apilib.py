960d4aaf893362ac50f5db90815477a722a65ffc
Ed-Davis <skootaman@btopenworld.com>
2024-03-08 03:50:24 +0000

    Added docstring to module(7)

--- apilib.py
+++ apilib.py
@@ -15,16 +15,19 @@ class Api:
         }
 
     def data(self, endpoint='ticker/24hr', querystring=None) -> dict:
+        """API Get Requests"""
         url = self.baseurl + endpoint
         response = requests.get(url=url, headers=self.headers, params=querystring)
         return response.json()
 
     @staticmethod
     def mocked_data() -> dict:
+        """capture mocked data from file"""
         with open('example.json') as json_file:
             return json.load(json_file)
 
     @staticmethod
     def top3(body_data: dict) -> list:
+        """Sort the data"""
         ordered = sorted(body_data, key=lambda x: x['priceChangePercent'])
         return ordered[-3:]
